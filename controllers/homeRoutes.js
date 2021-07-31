21269866a844f90c9b8be0b71758e45c275af1db
ra8200 <ra8200@hotmail.com>
2021-07-31 14:56:09 -0400

    can't update yet

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -40,7 +40,7 @@ router.get('/blog/:id', async (req, res) => {
     });
 
     const blog = blogData.get({ plain: true });
-
+    console.log(blog);
     res.render('blog', {
       ...blogs,
       logged_in: req.session.logged_in,
